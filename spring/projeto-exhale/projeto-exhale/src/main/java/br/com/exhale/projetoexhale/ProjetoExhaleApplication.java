50db7b220f158f9332758f71977d4ec2ef4c5a61
unknown <rubensnascimento74>
2020-09-06 20:57:12 -0700

    criacao springbot

---
+++ spring/projeto-exhale/projeto-exhale/src/main/java/br/com/exhale/projetoexhale/ProjetoExhaleApplication.java
@@ -0,0 +1,13 @@
+package br.com.exhale.projetoexhale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoExhaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExhaleApplication.class, args);
+	}
+
+}
---
+++ spring/projeto-exhale/projeto-exhale/src/test/java/br/com/exhale/projetoexhale/ProjetoExhaleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exhale.projetoexhale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoExhaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
